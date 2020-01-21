7f9a09585919f3ff341f604542929fe46c5fb14f
lomeat <lom3at@gmail.com>
2020-01-21 12:29:51 +0300

    Updates

---
+++ precache-manifest.3adf0ac7fb18eca820a54cfe98224af6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "647036313c9228b825442079c8af2bfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4f34dc589faa4c8e128",
+    "url": "/static/js/2.be214888.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.be214888.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6bf6fa3c6fac5168d36c",
+    "url": "/static/js/main.aba4f440.chunk.js"
+  },
+  {
+    "revision": "0e4071e29f01c87dd78d",
+    "url": "/static/js/runtime-main.09c91e61.js"
+  },
+  {
+    "revision": "9886ec40b43359bc4663a531740aac28",
+    "url": "/static/media/example.9886ec40.jpg"
+  },
+  {
+    "revision": "c535fed7c76ea56ebdfc2b7549324a1a",
+    "url": "/static/media/logo.c535fed7.png"
+  }
+]);
